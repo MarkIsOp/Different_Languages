6424da8cdaca7f00745e327c8588c44c69e721d0
Mark Kleshkov <markkleshkov@mail.ru>
2024-08-21 01:50:02 +0800

    editing test_items

---
+++ test_items.py
@@ -0,0 +1,7 @@
+from selenium.webdriver.common.by import By
+import time
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_add_to_chart(browser):
+    browser.get(link)
+    browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
