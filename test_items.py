0aa0b40b7c77c842c2ee354bae6a3418df5190fe
Mark Kleshkov <markkleshkov@mail.ru>
2024-08-21 02:21:45 +0800

    final edit

--- test_items.py
+++ test_items.py
@@ -5,3 +5,4 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_add_to_chart(browser):
     browser.get(link)
     browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
+    time.sleep(5)
